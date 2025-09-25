f428142e12211acf28f9ef31928fb4220d23149c
abc <abc@example.com>
2025-09-25 03:39:59 +0300

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,13 +4,14 @@ import Highlights   from "./components/Highlights.jsx";
 import Colors from "./components/Colors1.jsx";
 import Titanium from "./components/Titanium.jsx";
 import Control from "./components/Control.jsx";
-import VisualI  from "./components/VisualI.jsx";  
+import VisualI  from "./components/VisualI.jsx";
 import Sizes from "./components/Sizes.jsx";
 import Section1 from "./components/Section1.jsx";
 import Section2 from "./components/Section2.jsx";
 import Section3 from "./components/Section3.jsx";
 import Promo from "./components/Promo.jsx";
 import Photographic from "./components/Photographic.jsx";
+
 function App() {
   
 
